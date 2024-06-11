559970dc79785b0f65708c34670918491379816b
Krishna <krishnathescienceguy@outlook.com>
2024-06-11 14:38:28 -0500

    loggers finally work

--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,6 @@ use simple_logger::SimpleLogger;
 
 fn main(){
     println!("Hello World. Mukund if you touch this project again you're done");
+    SimpleLogger::new().init().unwrap();
     info!("Loggers are functional");
 }
